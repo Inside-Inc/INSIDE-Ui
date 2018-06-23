de11303cd42429db0d2e82363c9402c1c2d0cbae
Nehal-Mohsen <erma2009@windowslive.com>
2018-06-24 01:21:24 +0200

    edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,13 @@ import { NavComponent } from './nav/nav.component';
 import { ClientSignUpHeaderComponent } from './client-sign-up/client-sign-up-header/client-sign-up-header.component';
 import { ClientSignUpContentComponent } from './client-sign-up-content/client-sign-up-content.component';
 import { SignInComponent } from './sign-in/sign-in.component';
-import { SignInHeaderComponent } from './sign-in/sign-in-header/sign-in-header.component';
-
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     ClientSignUpHeaderComponent,
     ClientSignUpContentComponent,
-    SignInComponent,
-    SignInHeaderComponent
+    SignInComponent
   ],
   imports: [
     BrowserModule
