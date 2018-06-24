edcc3fc177d63048ed1147bbac5bec26247cb65a
Mohamed Elbehary <melbehary95@gmail.com>
2018-06-24 19:52:31 +0200

    edit

--- src/app/client-sign-up/client-sign-up-content/client-sign-up-content.component.ts
+++ src/app/client-sign-up/client-sign-up-content/client-sign-up-content.component.ts
@@ -13,23 +13,11 @@ export class ClientSignUpContentComponent implements OnInit {
   ngOnInit() {
   }
   stepsSwitcher(){ // Switch btween the steps
-     var step1 = document.getElementById('step1'),
-          step2 = document.getElementById('step2'),
-          step3 = document.getElementById('step3'),
-          stepsSlider = document.getElementById('stepsSlider');
-     var stepsArr = [step1, step2, step3];
-     // var position = stepsSlider.setAttribute('rightPosition', '0');
+     var stepsSlider = document.getElementById('stepsSlider');
      var position = stepsSlider.getAttribute('rightPosition');
-     var addRight = position + 880;
+     var positionInt = Number(position);
+     var addRight = positionInt + 880;
      stepsSlider.style.right= addRight+'px';
      var position = stepsSlider.setAttribute('rightPosition', addRight);
-     //      stepsSlider.style.left = x_pos+'px';
-     // console.log(stepsSlider.x_pos);
-      // console.log(this.stepsCounter);
-      // var step = stepsArr[this.stepsCounter];
-      // var next = stepsArr[this.stepsCounter+1];
-      // step.className = 'hidden-test';
-      // next.className = '';
-      //  this.stepsCounter += 1;
  }
 }
