edb223019d3f4f8cafd175445d7655be9eeaa9fd
Mohamed Elbehary <melbehary95@gmail.com>
2018-07-01 19:58:13 +0200

    validation 0.4
    validation
    header icon Green & Red
    back buttons
    back button functions
    err messages sliding

--- src/app/client-sign-up/client-sign-up-content/client-sign-up-content.js
+++ src/app/client-sign-up/client-sign-up-content/client-sign-up-content.js
@@ -4,13 +4,17 @@
           userIdCalB = document.getElementById('userIdCalB'),
           userContCalA = document.getElementById('userContCalA'),
           userContCalB = document.getElementById('userContCalB'),
+          userPaymentA = document.getElementById('userPaymentA'),
           nextButtStep1 = document.getElementById('nextButtStep1'),
           nextButtStep2 = document.getElementById('nextButtStep2'),
+          backButtStep2 = document.getElementById('backButtStep2'),
+          BackButtStep3 = document.getElementById('BackButtStep3'),
           errMessage = document.getElementById('errMessage'),
           errMessages= document.getElementById('errMessages');
      var stepsSlider = document.getElementById('stepsSlider'),
           headerTitle = document.getElementById('headerTitle'),
           stepsNumberHead = document.getElementById('stepsNumberHead'),
+          step1Icon = document.getElementById('step1Icon'),
           stepLine1 = document.getElementById('stepLine1'),
           step2Icon = document.getElementById('step2Icon'),
           stepLine2 = document.getElementById('stepLine2'),
@@ -98,14 +102,29 @@
                column.children[i].removeAttribute('disabled');
           }
      } // Enable inputs
+     function iconErr(iconStep){
+          iconStep.classList.add('red-bc');
+          iconStep.classList.remove('gray-bc');
+          iconStep.classList.remove('green-bc');
+     }
+     function iconGo(iconStep){
+          iconStep.classList.add('green-bc');
+          iconStep.classList.remove('gray-bc');
+          iconStep.classList.remove('red-bc');
+     } // make steps icon as green
+     function iconBack(iconStep){
+          iconStep.classList.add('gray-bc');
+          iconStep.classList.remove('green-bc');
+          iconStep.classList.remove('red-bc');
+     } // make steps icon as gray
      function stepsSwitcher(){ // Switch btween the steps
              stepsCounter += 880;
              if(stepsCounter == 880){  // user contact
                   headerTitle.style.opacity = '.0';
                   title = 'User Contact';
-                  headerTitle.innerHTML = title;
                   step2Icon.classList.remove('hidden');
                   setTimeout(function(){
+                      headerTitle.innerHTML = title;
                       headerTitle.classList.add('tra-a-2');
                       headerTitle.style.opacity = '1';
                       stepLine1.style.width = '100px';
@@ -114,9 +133,9 @@
                   headerTitle.classList.remove('tra-a-2');
                   headerTitle.style.opacity = '.0';
                   title = 'User Payment';
-                  headerTitle.innerHTML = title;
                   step3Icon.classList.remove('hidden');
                   setTimeout(function(){
+                      headerTitle.innerHTML = title;
                       headerTitle.classList.add('tra-a-2');
                       headerTitle.style.opacity = '1';
                       stepLine2.style.width = '100px';
@@ -124,17 +143,66 @@
              }
              stepsSlider.style.right= stepsCounter+'px';
      } // steps switcher
-     function runSwitcher(){setTimeout(function(){
+     function moveErrMessForwoards(){
           errMessages.classList.add('hidden-test');
           errMessages.classList.remove('errMessages');
           errMessages.style.right= errMessageRightPos - 110 + 'px';
           errMessageRightPos -= 110;
-          stepsSwitcher();
-     },400);} // steps switcher on run time
+     } // move Error message forwoards [Next Butt]
+     function slideErrMess(){
+          errMessage.style.opacity='0';
+          errMessage.style.left='-330px';
+     } // slide Error message back
+     function moveErrMessBackwoards(){
+          errMessages.classList.add('hidden-test');
+          errMessages.classList.remove('errMessages');
+          errMessages.style.right= errMessageRightPos + 110 + 'px';
+          errMessageRightPos += 110;
+     } // Move Error Message Backwoards
+     function runSwitcher(){
+          setTimeout(function(){
+               moveErrMessForwoards();
+               stepsSwitcher();
+          },400);
+     } // steps switcher on run time
+     function stepsBack(){
+          stepsCounter -= 880;
+          if(stepsCounter == 0){  // user ID
+               headerTitle.style.opacity = '.0';
+               title = 'User ID';
+               step2Icon.classList.add('hidden');
+               setTimeout(function(){
+                    headerTitle.innerHTML = title;
+                   headerTitle.classList.add('tra-a-2');
+                   headerTitle.style.opacity = '1';
+                   stepLine1.style.width = '0';
+               },200);
+          }else if (stepsCounter > 0){
+               headerTitle.classList.remove('tra-a-2');
+               headerTitle.style.opacity = '.0';
+               title = 'User Contact';
+               step3Icon.classList.add('hidden');
+               setTimeout(function(){
+                   headerTitle.innerHTML = title;
+                   headerTitle.classList.add('tra-a-2');
+                   headerTitle.style.opacity = '1';
+                   stepLine2.style.width = '0';
+               },200);
+          }
+          stepsSlider.style.right= stepsCounter+'px';
+     } // steps back
+     function runStepsBack(){
+          slideErrMess();
+          setTimeout(function(){
+               moveErrMessBackwoards();
+               stepsBack();
+          },400);
+     } // run steps switcher to back
 //  EVENTS ---
      nextButtStep1.onclick =  function(){
           runValid(userIdCalA);
           removeAlert(userIdCalB);
+          iconErr(step1Icon);
           if(runValid(userIdCalA) == true){
                runValid(userIdCalB);
                if(runValid(userIdCalB) == true){
@@ -143,6 +211,7 @@
                          passcode();
                          if(passcode() == true){
                               runSwitcher();
+                              iconGo(step1Icon);
                               enableInps(userContCalA);
                               enableInps(userContCalB);
                               disabledInps(userIdCalA);
@@ -155,16 +224,35 @@
      nextButtStep2.onclick =  function(){
           runValid(userContCalA);
           removeAlert(userContCalB);
+          iconErr(step2Icon);
           if(runValid(userContCalA) == true){
                runValid(userContCalB);
                if(runValid(userContCalB) == true){
                     runSwitcher();
-                    enableInps(userContCalA);
-                    enableInps(userContCalB);
+                    iconGo(step2Icon);
+                    enableInps(userPaymentA);
                     disabledInps(userContCalA);
                     disabledInps(userContCalB);
                }
           }
      } // Click on next button step Two STEP(-2-)
+     backButtStep2.onclick = function (){
+          runStepsBack();
+          iconBack(step2Icon);
+          removeAlert(userContCalA);
+          removeAlert(userContCalB);
+          enableInps(userIdCalA);
+          enableInps(userIdCalB);
+          disabledInps(userContCalA);
+          disabledInps(userContCalB);
+     }
+     BackButtStep3.onclick = function(){
+          runStepsBack();
+          iconBack(step3Icon);
+          removeAlert(userPaymentA);
+          enableInps(userContCalA);
+          enableInps(userContCalB);
+          disabledInps(userPaymentA);
+     }
 // onload END ---
 };
