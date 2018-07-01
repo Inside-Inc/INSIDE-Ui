91669f790ae58df51c93b07a1f6728fa4510c530
Mohamed Elbehary <melbehary95@gmail.com>
2018-07-01 14:45:40 +0200

    validation 0.3
    Password & Re-Password validate
    Email validate

--- src/app/client-sign-up/client-sign-up-content/client-sign-up-content.js
+++ src/app/client-sign-up/client-sign-up-content/client-sign-up-content.js
@@ -15,7 +15,10 @@
           step2Icon = document.getElementById('step2Icon'),
           stepLine2 = document.getElementById('stepLine2'),
           step3Icon = document.getElementById('step3Icon'),
-          nextButt = document.getElementById('nextButt');
+          nextButt = document.getElementById('nextButt'),
+          eMail = document.getElementById('eMail'),
+          password = document.getElementById('password'),
+          rePassword = document.getElementById('rePassword');
       var stepsCounter = 0;
       var errMessageRightPos = 180;
       var title = 'User ID';
@@ -30,7 +33,38 @@
                }
           }
           return false;
-     } // Validation of inputs
+     } // Requirment of inputs
+     function ValidateEmail(){
+          if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(eMail.value)){
+               errMessage.style.opacity='0';
+               errMessage.style.left='-330px';
+               return true;
+          }else{
+               var name = eMail.getAttribute('name');
+               errMessages.classList.remove('hidden-test');
+               errMessages.classList.add('errMessages');
+               errMessage.style.opacity='1';
+               errMessage.style.left = '0';
+               errMessage.innerHTML = 'Invalid E-Mail address !';
+               eMail.style.borderLeft = '8px solid #F5664B';
+          }
+
+     } // valedate Email Address
+     function passcode(){
+          if(password.value === rePassword.value){
+               errMessage.style.opacity='0';
+               errMessage.style.left='-330px';
+               return true;
+          }else{
+               var name = rePassword.getAttribute('name');
+               errMessages.classList.remove('hidden-test');
+               errMessages.classList.add('errMessages');
+               errMessage.style.opacity='1';
+               errMessage.style.left = '0';
+               errMessage.innerHTML = 'Password dose not match !';
+               rePassword.style.borderLeft = '8px solid #F5664B';
+          }
+     } // Matching btween Passowrd and rePassword
      function removeAlert(column){
           for(var i=0; i<column.children.length; i++){
                column.children[i].style.borderLeft = '0';
@@ -64,7 +98,7 @@
                column.children[i].removeAttribute('disabled');
           }
      } // Enable inputs
-      function stepsSwitcher(){ // Switch btween the steps
+     function stepsSwitcher(){ // Switch btween the steps
              stepsCounter += 880;
              if(stepsCounter == 880){  // user contact
                   headerTitle.style.opacity = '.0';
@@ -104,16 +138,21 @@
           if(runValid(userIdCalA) == true){
                runValid(userIdCalB);
                if(runValid(userIdCalB) == true){
-                    runSwitcher();
-                    enableInps(userContCalA);
-                    enableInps(userContCalB);
-                    disabledInps(userIdCalA);
-                    disabledInps(userIdCalB);
+                    ValidateEmail();
+                    if(ValidateEmail() == true){
+                         passcode();
+                         if(passcode() == true){
+                              runSwitcher();
+                              enableInps(userContCalA);
+                              enableInps(userContCalB);
+                              disabledInps(userIdCalA);
+                              disabledInps(userIdCalB);
+                         }
+                    }
                }
           }
      } // Click on next button step One STEP(-1-)
      nextButtStep2.onclick =  function(){
-          // console.log(userContCalA);
           runValid(userContCalA);
           removeAlert(userContCalB);
           if(runValid(userContCalA) == true){
